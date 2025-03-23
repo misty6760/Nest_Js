df6e5240d1590fab0f2dd68369b347fbbaec4d75
misty6760 <kinghojoon12@gmail.com>
2025-03-23 15:29:53 +0900

    edit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
+import { DatabaseService } from './database/database.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ChatModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
