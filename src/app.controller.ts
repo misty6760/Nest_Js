d7b69ec653eddd32dcdac512ec1824d3cb62003a
misty6760 <kinghojoon12@gmail.com>
2025-03-23 17:26:14 +0900

    edit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UnsupportedMediaTypeException } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllUsers() {
+    return this.appService.getAllUsers();
+  }
+
+  @Post()
+  createUser(@Body() userDto: { name: string; age: number }) {
+    return this.appService.createUser(userDto);
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id:string) {
+    return this.appService.getUser(id);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() userDto: { name: string; age: number }) {
+    return this.appService.updataUser(id, userDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id:string) {
+    return this.appService.deleteUser(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { use } from 'passport';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly users =[
+    {id: '1', name: 'John Doe', age: 30},
+    {id: '2', name: 'Jane Doe', age: 20},
+  ];
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  createUser(userDto: {name: string; age: number}) {
+    const newUser = {
+      id: (this.users.length + 1).toString(),
+      ...userDto,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  getUser(id: string) {
+    const user = this.users.find((user) => user, id === id);
+    if (!user) {
+      return { message: 'User not found'};
+    }
+    return user;
+  }
+
+  updataUser(id: string, userDto: { name: string; age: number}) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if(userIndex === -1) {
+      return { message: "User not found"};
+    }
+    this.users[userIndex] = { id, ...userDto};
+    return this.users[userIndex];
+  }
+
+  deleteUser(id:string) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if(userIndex === -1) {
+      return { message: "User not found" };
+    }
+    this.users.splice(userIndex, 1);
+    return { message: "User deleted" };
   }
 }
