adb6a326ba0009587c75ce1db50cf4dc45030c8b
misty6760 <kinghojoon12@gmail.com>
2025-03-23 15:46:49 +0900

    idk

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ChatModule } from './chat/chat.module';
 import { DatabaseService } from './database/database.service';
-<<<<<<< HEAD
 import { ConfigService } from './config/config.service';
-=======
->>>>>>> df6e5240d1590fab0f2dd68369b347fbbaec4d75
 
 @Module({
   imports: [AuthModule, UserModule, ChatModule],
   controllers: [AppController],
-<<<<<<< HEAD
   providers: [AppService, DatabaseService, ConfigService],
-=======
-  providers: [AppService, DatabaseService],
->>>>>>> df6e5240d1590fab0f2dd68369b347fbbaec4d75
 })
 export class AppModule {}
