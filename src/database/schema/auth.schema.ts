11592b9a94fcae0ffc41b8848a729f6f6cb0d36d
misty6760 <kinghojoon12@gmail.com>
2025-04-06 17:43:48 +0900

    modified:   src/database/schema/user.schema.ts new file:   src/database/schema/user.schema.ts

--- src/database/schema/auth.schema.ts
+++ src/database/schema/auth.schema.ts
@@ -3,19 +3,24 @@ import { HydratedDocument } from "mongoose";
 import { Permission } from "src/common/enums/permission.enum";
 import { v4 as uuidv4 } from "uuid";
 
+// 'Auth' 모델의 문서 타임을 정의
 export type AuthDocument = HydratedDocument<Auth>;
 
+// 'Auth' 스키마 정의
 @Schema({ timestamps: true, versionKey: false })
 export class Auth {
-    @Prop({ default: uuidv4 })
+    @Prop({ default: uuidv4 }) // uuidv4()를 기본값으로 설정 (unique 속성 제거)
     _id: string;
 
+    // 필수, 고유한 사용자명
     @Prop({ required: true, unique: true })
     username: string;
 
+    // 필수 비밀번호
     @Prop({ required: true })
     password: string;
 
+    // 필수,ㅡ 권한 열거형 (기본값: USER)
     @Prop({
         required: true,
         type: String,
@@ -24,15 +29,18 @@ export class Auth {
     })
     permission: Permission;
 
+    //닉네임임
     @Prop({ required: true })
     nickname: string;
 }
 
+// Auth 클래스를 바탕으로 Mongoose 스키마를 생성
 export const AuthSchema = SchemaFactory.createForClass(Auth);
 
+// save 시에 uid가 없으면 UUIDv4를 자동로 생성하여 할당하는 pre save 훅
 AuthSchema.pre('save', function (next) {
     if (!this.isNew) {
-        this._id = uuidv4();
+        this._id = uuidv4(); // uid가 없으면 UUIDv4를 생성하여 할당
     }
     next();
 });
