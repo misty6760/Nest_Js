8015ec7d660a03df9a7f0ec22d8249a6dbe5616c
misty6760 <kinghojoon12@gmail.com>
2025-04-06 17:21:40 +0900

    modified

--- src/database/schema/auth.schema.ts
+++ src/database/schema/auth.schema.ts
@@ -1,9 +1,38 @@
-import { Permission } from "src/common/enums/permission.enum"
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { Permission } from "src/common/enums/permission.enum";
+import { v4 as uuidv4 } from "uuid";
 
+export type AuthDocument = HydratedDocument<Auth>;
+
+@Schema({ timestamps: true, versionKey: false })
 export class Auth {
-    id: string;
+    @Prop({ default: uuidv4 })
+    _id: string;
+
+    @Prop({ required: true, unique: true })
     username: string;
+
+    @Prop({ required: true })
     password: string;
+
+    @Prop({
+        required: true,
+        type: String,
+        enum: Permission,
+        default: Permission.USER,
+    })
     permission: Permission;
+
+    @Prop({ required: true })
     nickname: string;
 }
+
+export const AuthSchema = SchemaFactory.createForClass(Auth);
+
+AuthSchema.pre('save', function (next) {
+    if (!this.isNew) {
+        this._id = uuidv4();
+    }
+    next();
+});
